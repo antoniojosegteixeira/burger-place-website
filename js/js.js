f464f36f82c418c8e085117106f1bbc9cb6c41f4
Antonio José <antoniojosegteixeira@gmail.com>
2021-06-24 16:35:59 -0400

    Map added

---
+++ js/js.js
@@ -0,0 +1,13 @@
+
+ // BING MAPS
+ function GetMap() {
+  const map = new Microsoft.Maps.Map("#myMap", {
+      credentials: "Arw-oMq_6wN6CB2Xf9TWcF-lXOCBrST2mKPFFNTkobmzyTDn8B0h81elt49EjMJ2",
+      center: new Microsoft.Maps.Location(-15.5992119,-56.0971276), 
+      mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+      zoom: 16
+  });
+
+  const center = map.getCenter();
+
+}
