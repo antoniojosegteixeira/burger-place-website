c0322f3a01288cc6093e5d24794300eb17bd69fa
Antonio José <antoniojosegteixeira@gmail.com>
2021-06-28 01:57:25 -0400

    Fade animations improved

--- js/js.js
+++ js/js.js
@@ -19,31 +19,56 @@ function GetMap() {
   map.entities.push(pin);
 }
 
-
-
 let scrollTop = document.documentElement.scrollTop;
 let screenHeight = screen.height;
-const menuWrapper = document.getElementById('menu');
-const menuContent = document.getElementById('menu-content')
 
+
+/// LISTENER FOR SCROLL
 window.addEventListener('scroll', () => {
   scrollTop = document.documentElement.scrollTop;
+  screenHeight = screen.height;
+  fadeElements(["about", "gallery", "infos"]);
+  fadeMenuItems();
+});
 
 
-  const menuHeight = document.getElementById('menu').offsetTop;
-  const menuShowHeight = menuHeight - (screenHeight / 2);
-  console.log(menuShowHeight);
+
+
+function fadeElements(target) {
+  target.map( (e) => {
+    const breakpoint = document.getElementById(e).offsetTop - (screenHeight * 0.7);
+    const targetElement = document.getElementById(e);
   
-  if(scrollTop >= menuShowHeight) {
-    console.log(menuShowHeight, scrollTop,'é maior')
-    menuContent.classList.remove('up-fade-reverse');
-    menuContent.classList.add('up-fade');
-  }
+    if(scrollTop >= breakpoint) {
+      console.log('maior')
+      targetElement.classList.remove('up-fade-reverse');
+      targetElement.classList.add('up-fade');
+    }
+  
+    if(scrollTop <= breakpoint) {
+      targetElement.classList.remove('up-fade');
+      targetElement.classList.add('up-fade-reverse');
+    }
+  });
+}
 
-  if(scrollTop < menuShowHeight) {
-    console.log(menuShowHeight, scrollTop,'é menor')
-    menuContent.classList.remove('up-fade');
-    menuContent.classList.add('up-fade-reverse');
 
+/// MENU TABLE ANIMATION
+const menu = document.getElementsByName('menu-item');
+function fadeMenuItems() {
+  const breakpoint = (document.getElementById('menu').offsetTop - (screenHeight * 0.6));
+  
+  if(scrollTop >= breakpoint) {
+    for(let i = 0; i < menu.length; i++) {
+      menu[i].style.animationDelay = `${i * 200}ms`;
+      menu[i].classList.add('menu-fade');
+    }
   }
-})
+  
+  if(scrollTop <= breakpoint) {
+    for(let i = 0; i < menu.length; i++) {
+      menu[i].classList.remove('menu-fade');
+    }
+  }
+}
+  
